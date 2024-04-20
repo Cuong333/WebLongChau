2d62c807dbb7662f423bcbdcc4b1e2540793d373
cuong333 <dangcuong9331@gmail.com>
2024-04-20 22:51:25 +0700

    Add,edit,delete.

--- Areas/Admin/Controllers/AdminController.cs
+++ Areas/Admin/Controllers/AdminController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using WebLongChau.Data;
 using WebLongChau.Models;
 namespace WebLongChau.Areas.Admin.Controllers
 {
@@ -8,15 +13,86 @@ namespace WebLongChau.Areas.Admin.Controllers
     [Route("admin/homeadmin")]
     public class AdminController : Controller
     {
+        NtLongChauContext db=new NtLongChauContext();
         [Route("")]
         [Route("index")]
         public IActionResult Index()
         {
             return View();
         }
-        //public IActionResult ListProduct()
-        //{
-        //    var listproduct =db
-        //}
+
+        // List Product
+        [Route("ListProduct")]
+        public IActionResult ListProduct()
+        {
+         var listproduct =db.Products.ToList();
+          return View(listproduct);
+        }
+        // Add Product
+        [Route("AddProduct")]
+        [HttpGet]
+
+        public IActionResult AddProduct()
+        {
+            ViewBag.SupplierId = new SelectList (db.Suppliers.ToList(), "SupplierId", "SupplierName");
+            ViewBag.CategoryId = new SelectList (db.Categories.ToList(), "CategoryId", "Name");
+            return View();
+        }
+        [Route("AddProduct")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult AddProduct(Product product) 
+        {
+            if (ModelState.IsValid != null)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("ListProduct");   
+            }
+            return View(product);
+        }
+
+        // Edit Product
+        [Route("EditProduct")]
+        [HttpGet]
+        public IActionResult EditProduct(int ProductId)
+        {
+            ViewBag.SupplierId = new SelectList(db.Suppliers.ToList(), "SupplierId", "SupplierName");
+            ViewBag.CategoryId = new SelectList(db.Categories.ToList(), "CategoryId", "Name");
+            var product = db.Products.Find(ProductId);
+            return View(product);
+        }
+        [Route("EditProduct")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditProduct(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("ListProduct","HomeAdmin");
+            }
+            return View(product);
+        }
+        // Delete Product
+        [HttpGet]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteProduct(int productId)
+        {
+            TempData["Message"] = "";
+            var product = db.Products.Find(productId);
+            if (product == null)
+            {
+                TempData["Message"] = "Can not delete product";
+                return NotFound();
+            }
+
+            db.Products.Remove(product);
+            db.SaveChanges();
+            TempData["Message"] = "Delete product successfull";
+            return RedirectToAction("ListProduct", "HomeAdmin");
+        }
+
     }
 }
