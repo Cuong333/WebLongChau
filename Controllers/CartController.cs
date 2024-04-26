e3f423f0a5795fa68d34abaf45553a4d253d04dd
thinhgcs210935 <129242283+thinhgcs210935@users.noreply.github.com>
2024-04-26 11:12:41 +0700

    Create -> Cartitem, CartController, Helpers -> SessionExtension, Trong Phan Program

---
+++ Controllers/CartController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebLongChau.Data;
+using WebLongChau.Models;
+using WebLongChau.Helpers;
+
+namespace WebLongChau.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly LongChauWebContext db;
+
+        public CartController(LongChauWebContext context)
+        {
+            db = context;
+        }
+        const string Cart_key = "MyCart";
+        public List<Cartitem> Cart => HttpContext.Session.Get<List<Cartitem>>(Cart_key) 
+            ?? new List<Cartitem>();
+        public IActionResult Index()
+        {
+            return View(Cart);
+        }
+        public IActionResult AddToCart(int ProductID, int Quantity = 1)
+        {
+            var Mycart = Cart;
+            var item = Mycart.SingleOrDefault(p => p.ProductID == ProductID);
+            /*if (item = null)
+            {
+                var Product = db.Products.SingleOrDefault(p => p.ProductId == ProductID);
+                if (Product = null)
+                {
+                    return NotFound;
+                }
+            }*/
+            return View();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -20,8 +20,16 @@ builder.Services.AddControllersWithViews();
 //        options.Password.RequireNonAlphanumeric = false;
 
 //    }).AddDefaultTokenProviders();
-    
+
 //builder.Services.AddSession();
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -39,6 +47,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseSession();
 
 app.UseAuthorization();
 
